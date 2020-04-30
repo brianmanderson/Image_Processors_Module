dbc03a9e044660ed29885b6661161c3de259cb97
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-04-30 17:17:35 -0500

    major revisions

--- Image_Processors_TFRecord.py
+++ Image_Processors_TFRecord.py
@@ -24,8 +24,8 @@ def get_bounding_boxes(annotation_handle,value):
     connected_image = Connected_Component_Filter.Execute(thresholded_image)
     stats.Execute(connected_image)
     bounding_boxes = [stats.GetBoundingBox(l) for l in stats.GetLabels()]
-    volumes = np.asarray([stats.GetPhysicalSize(l) for l in stats.GetLabels()]).astype('float32')
-    return bounding_boxes, volumes
+    num_voxels = np.asarray([stats.GetNumberOfPixels(l) for l in stats.GetLabels()]).astype('float32')
+    return bounding_boxes, num_voxels
 
 
 class Image_Processor(object):
@@ -48,19 +48,86 @@ class Clip_Images_By_Extension(Image_Processor):
         return input_features
 
 
+class Split_Disease_Into_Cubes(Image_Processor):
+    def __init__(self, disease_annotation=None, cube_size=(16, 100, 100), min_voxel_volume=0, max_voxels=np.inf):
+        '''
+        :param disease_annotation: integer for disease annotation
+        '''
+        self.disease_annotation = disease_annotation
+        self.cube_size = np.asarray(cube_size)
+        self.min_voxel_volume = min_voxel_volume
+        self.max_voxels = max_voxels
+        assert disease_annotation is not None, 'Provide an integer for what is disease'
+
+    def parse(self, input_features):
+        if 'bounding_boxes_{}'.format(self.disease_annotation) not in input_features:
+            Add_Bounding_Box = Add_Bounding_Box_Indexes([self.disease_annotation], add_to_dictionary=False)
+            input_features = Add_Bounding_Box.parse(input_features)
+        if 'bounding_boxes_{}'.format(self.disease_annotation) in input_features:
+            bounding_boxes = input_features['bounding_boxes_{}'.format(self.disease_annotation)]
+            voxel_volumes = input_features['voxel_volumes_{}'.format(self.disease_annotation)]
+            del input_features['voxel_volumes_{}'.format(self.disease_annotation)]
+            del input_features['bounding_boxes_{}'.format(self.disease_annotation)]
+            image_base = input_features['image']
+            annotation_base = input_features['annotation']
+            out_features = OrderedDict()
+            for cube_index, [box, voxels] in enumerate(zip(bounding_boxes, voxel_volumes)):
+                if voxels < self.min_voxel_volume or voxels > self.max_voxels:
+                    continue
+                z_start, z_stop, r_start, r_stop, c_start, c_stop = add_bounding_box_to_dict(box, return_indexes=True)
+                box_size = [z_stop - z_start, r_stop - r_start, c_stop - c_start]
+                remainders = np.asarray([self.cube_size[i] - box_size[i] % self.cube_size[i]
+                                         if box_size[i] % self.cube_size[i] != 0 else 0 for i in range(3)])
+                z_start, z_stop, r_start, r_stop, c_start, c_stop = expand_box_indexes(z_start, z_stop, r_start, r_stop,
+                                                                                       c_start, c_stop,
+                                                                                       annotation_shape=
+                                                                                       annotation_base.shape,
+                                                                                       bounding_box_expansion=
+                                                                                       remainders//2+1)
+                image = image_base[z_start:z_stop, r_start:r_stop, c_start:c_stop]
+                annotation = annotation_base[z_start:z_stop, r_start:r_stop, c_start:c_stop]
+
+                stack = [np.stack([image,annotation],axis=0)]
+                for axis in range(3):
+                    output = []
+                    for i in stack:
+                        split = i.shape[axis+1] // self.cube_size[axis]
+                        if split > 1:
+                            output += np.array_split(i, split, axis=axis+1)
+                        else:
+                            output += [i]
+                    stack = output
+                for box_index, cube in enumerate(stack):
+                    temp_feature = OrderedDict()
+                    temp_feature['image'] = cube[0][:self.cube_size[0]]
+                    temp_feature['annotation'] = cube[1][:self.cube_size[0]]
+                    for key in input_features:  # Bring along anything else we care about
+                        if key not in temp_feature.keys():
+                            temp_feature[key] = input_features[key]
+                    out_features['Disease_Box_{}_{}'.format(cube_index, box_index)] = temp_feature
+            return out_features
+        return input_features
+
 class Distribute_into_3D(Image_Processor):
-    def __init__(self, max_z=np.inf, mirror_small_bits=True):
+    def __init__(self, min_z=0, max_z=np.inf, max_rows=np.inf, max_cols=np.inf, mirror_small_bits=True):
         self.max_z = max_z
+        self.min_z = min_z
+        self.max_rows, self.max_cols = max_rows, max_cols
         self.mirror_small_bits = mirror_small_bits
 
     def parse(self, input_features):
-        out_features = {}
+        out_features = OrderedDict()
         start_chop = 0
         image_base = input_features['image']
         annotation_base = input_features['annotation']
         image_path = input_features['image_path']
         spacing = input_features['spacing']
         z_images_base, rows, cols = image_base.shape
+        if self.max_rows != np.inf:
+            rows = min([rows,self.max_rows])
+        if self.max_cols != np.inf:
+            cols = min([cols,self.max_cols])
+        image_base, annotation_base = image_base[:, :rows, :cols], annotation_base[:, :rows, :cols]
         step = min([self.max_z, z_images_base])
         for index in range(z_images_base // step + 1):
             image_features = OrderedDict()
@@ -68,15 +135,15 @@ class Distribute_into_3D(Image_Processor):
                 continue
             image = image_base[start_chop:start_chop + step, ...]
             annotation = annotation_base[start_chop:start_chop + step, ...]
-            if image.shape[0] < step:
+            if image.shape[0] < max([step, self.min_z]):
                 if self.mirror_small_bits:
-                    while image.shape[0] < step:
+                    while image.shape[0] < max([step, self.min_z]):
                         mirror_image = np.flip(image, axis=0)
                         mirror_annotation = np.flip(annotation, axis=0)
                         image = np.concatenate([image, mirror_image], axis=0)
                         annotation = np.concatenate([annotation, mirror_annotation], axis=0)
-                    image = image[:step]
-                    annotation = annotation[:step]
+                    image = image[:max([step, self.min_z])]
+                    annotation = annotation[:max([step, self.min_z])]
             start, stop = get_start_stop(annotation, extension=0)
             image_features['image_path'] = image_path
             image_features['image'] = image
@@ -93,8 +160,9 @@ class Distribute_into_3D(Image_Processor):
 
 
 class Distribute_into_2D(Image_Processor):
+
     def parse(self, input_features):
-        out_features = {}
+        out_features = OrderedDict()
         image = input_features['image']
         annotation = input_features['annotation']
         image_path = input_features['image_path']
@@ -147,6 +215,15 @@ class Normalize_to_annotation(Image_Processor):
         input_features['image'] = images
         return input_features
 
+def expand_box_indexes(z_start, z_stop, r_start, r_stop, c_start, c_stop, annotation_shape, bounding_box_expansion):
+    z_start = max([0, z_start - bounding_box_expansion[0]])
+    z_stop = min([annotation_shape[0], z_stop + bounding_box_expansion[0]])
+    r_start = max([0, r_start - bounding_box_expansion[1]])
+    r_stop = min([annotation_shape[1], r_stop + bounding_box_expansion[1]])
+    c_start = max([0, c_start - bounding_box_expansion[2]])
+    c_stop = min([annotation_shape[2], c_stop + bounding_box_expansion[2]])
+    return z_start, z_stop, r_start, r_stop, c_start, c_stop
+
 
 class Box_Images(Image_Processor):
     def __init__(self, wanted_vals_for_bbox=None,
@@ -161,6 +238,7 @@ class Box_Images(Image_Processor):
         self.bounding_box_expansion = bounding_box_expansion
         self.power_val_z, self.power_val_r, self.power_val_c = power_val_z, power_val_r, power_val_c
         self.min_images, self.min_rows, self.min_cols = min_images, min_rows, min_cols
+
     def parse(self, input_features):
         annotation = input_features['annotation']
         image = input_features['image']
@@ -175,12 +253,11 @@ class Box_Images(Image_Processor):
             r_stop = input_features['bounding_boxes_r_stop_{}'.format(val)]
             c_stop = input_features['bounding_boxes_c_stop_{}'.format(val)]
 
-            z_start = max([0, z_start - self.bounding_box_expansion[0]])
-            z_stop = min([z_stop + self.bounding_box_expansion[0], annotation.shape[0]])
-            r_start = max([0, r_start - self.bounding_box_expansion[1]])
-            r_stop = min([annotation.shape[1], r_stop + self.bounding_box_expansion[1]])
-            c_start = max([0, c_start - self.bounding_box_expansion[2]])
-            c_stop = min([annotation.shape[2], c_stop + self.bounding_box_expansion[2]])
+            z_start, z_stop, r_start, r_stop, c_start, c_stop = expand_box_indexes(z_start, z_stop, r_start, r_stop,
+                                                                                   c_start, c_stop,
+                                                                                   annotation_shape=annotation.shape,
+                                                                                   bounding_box_expansion=
+                                                                                   self.bounding_box_expansion)
 
             z_total, r_total, c_total = z_stop - z_start, r_stop - r_start, c_stop - c_start
             remainder_z, remainder_r, remainder_c = self.power_val_z - z_total % self.power_val_z if z_total % self.power_val_z != 0 else 0, \
@@ -208,31 +285,40 @@ class Box_Images(Image_Processor):
 
 
 class Add_Bounding_Box_Indexes(Image_Processor):
-    def __init__(self, wanted_vals_for_bbox=None):
+    def __init__(self, wanted_vals_for_bbox=None, add_to_dictionary=False):
         '''
         :param wanted_vals_for_bbox: a list of values in integer form for bboxes
         '''
         assert type(wanted_vals_for_bbox) is list, 'Provide a list for bboxes'
         self.wanted_vals_for_bbox=wanted_vals_for_bbox
+        self.add_to_dictionary = add_to_dictionary
 
     def parse(self, input_features):
         annotation = input_features['annotation']
         for val in self.wanted_vals_for_bbox:
             slices = np.where(annotation == val)
             if slices:
-                bounding_boxes, volumes = get_bounding_boxes(sitk.GetImageFromArray(annotation), val)
-                bounding_boxes = bounding_boxes[0]
-                volumes = volumes[0]
-                c_start, r_start, z_start, c_stop, r_stop, z_stop = bounding_boxes
-                z_stop, r_stop, c_stop = z_start + z_stop, r_start + r_stop, c_start + c_stop
-                input_features['bounding_boxes_z_start_{}'.format(val)] = z_start
-                input_features['bounding_boxes_r_start_{}'.format(val)] = r_start
-                input_features['bounding_boxes_c_start_{}'.format(val)] = c_start
-                input_features['bounding_boxes_z_stop_{}'.format(val)] = z_stop
-                input_features['bounding_boxes_r_stop_{}'.format(val)] = r_stop
-                input_features['bounding_boxes_c_stop_{}'.format(val)] = c_stop
-                input_features['volumes_{}'.format(val)] = volumes
+                bounding_boxes, voxel_volumes = get_bounding_boxes(sitk.GetImageFromArray(annotation), val)
+                input_features['voxel_volumes_{}'.format(val)] = voxel_volumes
+                input_features['bounding_boxes_{}'.format(val)] = bounding_boxes
+                input_features = add_bounding_box_to_dict(input_features=input_features, bounding_box=bounding_boxes[0],
+                                                          val=val, return_indexes=False, add_to_dictionary=self.add_to_dictionary)
         return input_features
+
+
+def add_bounding_box_to_dict(bounding_box, input_features=None, val=None, return_indexes=False, add_to_dictionary=False):
+    c_start, r_start, z_start, c_stop, r_stop, z_stop = bounding_box
+    z_stop, r_stop, c_stop = z_start + z_stop, r_start + r_stop, c_start + c_stop
+    if return_indexes:
+        return z_start, z_stop, r_start, r_stop, c_start, c_stop
+    if add_to_dictionary:
+        input_features['bounding_boxes_z_start_{}'.format(val)] = z_start
+        input_features['bounding_boxes_r_start_{}'.format(val)] = r_start
+        input_features['bounding_boxes_c_start_{}'.format(val)] = c_start
+        input_features['bounding_boxes_z_stop_{}'.format(val)] = z_stop
+        input_features['bounding_boxes_r_stop_{}'.format(val)] = r_stop
+        input_features['bounding_boxes_c_stop_{}'.format(val)] = c_stop
+    return input_features
 
 
 if __name__ == '__main__':
