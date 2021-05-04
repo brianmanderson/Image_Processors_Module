8c90a0a5d274894fdf9071b79766d3fc380ff17d
markba122@gmail.com <bmanderson@mdanderson.org>
2021-05-04 09:09:56 -0500

    adding the record writer locally

--- src/Processors/TFRecordWriter.py
+++ src/Processors/TFRecordWriter.py
@@ -1,10 +1,28 @@
 __author__ = 'Brian M Anderson'
 # Created on 5/4/2021
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import MakeTFRecordProcessors as Processors
 import tensorflow as tf
 import pickle
 import os
 from _collections import OrderedDict
 import numpy as np
+from threading import Thread
+import copy
+from multiprocessing import cpu_count
+from queue import *
+
+
+def _check_keys_(input_features, keys):
+    if type(keys) is list or type(keys) is tuple:
+        for key in keys:
+            assert key in input_features.keys(), 'Make sure the key you are referring to is present in the features, ' \
+                                                 '{} was not found'.format(key)
+    else:
+        assert keys in input_features.keys(), 'Make sure the key you are referring to is present in the features, ' \
+                                              '{} was not found'.format(keys)
 
 
 def save_obj(path, obj):  # Save almost anything.. dictionary, list, etc.
@@ -121,6 +139,33 @@ def dictionary_to_tf_record(filename, input_features):
     return {}
 
 
+def get_features(features, image_processors=None, verbose=0, record_writer=None):
+    if image_processors is not None:
+        for image_processor in image_processors:
+            features, _ = down_dictionary(features, OrderedDict(), 0)
+            if verbose:
+                print(image_processor)
+            for key in features.keys():
+                features[key] = image_processor.pre_process(features[key])
+        features, _ = down_dictionary(features, OrderedDict(), 0)
+    if record_writer is not None:
+        record_writer.write_records(features)
+
+
+def down_dictionary(input_dictionary, out_dictionary=None, out_index=0):
+    if out_dictionary is None:
+        out_dictionary = OrderedDict()
+    for key in input_dictionary.keys():
+        data = input_dictionary[key]
+        if type(data) is dict or type(data) is OrderedDict:
+            out_dictionary, out_index = down_dictionary(input_dictionary[key], out_dictionary, out_index)
+        else:
+            out_dictionary['Example_{}'.format(out_index)] = input_dictionary
+            out_index += 1
+            return out_dictionary, out_index
+    return out_dictionary, out_index
+
+
 class RecordWriter(object):
     def __init__(self, out_path, file_name_key='file_name', rewrite=False, **kwargs):
         self.file_name_key = file_name_key
@@ -170,28 +215,105 @@ class RecordWriterRecurrence(RecordWriter):
                 dictionary_to_tf_record(filename=filename, input_features={'out_example': example})
 
 
-def get_features(features, image_processors=None, verbose=0, record_writer=None):
-    if image_processors is not None:
-        for image_processor in image_processors:
-            features, _ = down_dictionary(features, OrderedDict(), 0)
-            if verbose:
-                print(image_processor)
-            for key in features.keys():
-                features[key] = image_processor.pre_process(features[key])
-        features, _ = down_dictionary(features, OrderedDict(), 0)
-    if record_writer is not None:
-        record_writer.write_records(features)
-
-
-def down_dictionary(input_dictionary, out_dictionary=None, out_index=0):
-    if out_dictionary is None:
-        out_dictionary = OrderedDict()
-    for key in input_dictionary.keys():
-        data = input_dictionary[key]
-        if type(data) is dict or type(data) is OrderedDict:
-            out_dictionary, out_index = down_dictionary(input_dictionary[key], out_dictionary, out_index)
+def worker_def(a):
+    q = a[0]
+    base_class = serialize_example
+    while True:
+        item = q.get()
+        if item is None:
+            break
         else:
-            out_dictionary['Example_{}'.format(out_index)] = input_dictionary
-            out_index += 1
-            return out_dictionary, out_index
-    return out_dictionary, out_index
+            try:
+                base_class(**item)
+            except:
+                print('Failed? {}'.format(item))
+            q.task_done()
+
+
+def return_data_dict(niftii_path):
+    data_dict = {}
+    image_files = [i for i in os.listdir(niftii_path) if i.find('Overall_Data') == 0]
+    for file in image_files:
+        iteration = file.split('_')[-1].split('.')[0]
+        data_dict[iteration] = {'image_path': os.path.join(niftii_path, file),
+                                'file_name': '{}.tfrecord'.format(file.split('.nii')[0])}
+
+    annotation_files = [i for i in os.listdir(niftii_path) if i.find('Overall_mask') == 0]
+    for file in annotation_files:
+        iteration = file.split('_y')[-1].split('.')[0]
+        data_dict[iteration]['annotation_path'] = os.path.join(niftii_path, file)
+    return data_dict
+
+
+def parallel_record_writer(niftii_path, out_path=None, rewrite=False, thread_count=int(cpu_count() * .5),
+                           max_records=np.inf, is_3D=True, extension=np.inf, image_processors=None,
+                           special_actions=False, verbose=False, file_parser=None, debug=False, recordwriter=None):
+    """
+    :param niftii_path: path to where Overall_Data and mask files are located
+    :param out_path: path that we will write records to
+    :param rewrite: Do you want to rewrite old records? True/False
+    :param thread_count: specify 1 if debugging
+    :param max_records: Can specify max number of records, for debugging purposes
+    :param extension: extension above and below annotation, recommend np.inf for validation and test
+    :param is_3D: Take the whole patient or break up into 2D images
+    :param image_processors: a list of image processes that can take the image and annotation dictionary,
+        see Image_Processors, TF_Record
+    :param special_actions: if you're doing something special and don't want Add_Images_And_Annotations
+    :param verbose: Binary, print processors as they go
+    :return:
+    """
+    if out_path is None:
+        out_path = niftii_path
+    if image_processors is None:
+        if is_3D:
+            image_processors = [Processors.Add_Images_And_Annotations(),
+                                Processors.Clip_Images_By_Extension(extension=extension),
+                                Processors.Distribute_into_3D()]
+        else:
+            image_processors = [Processors.Add_Images_And_Annotations(),
+                                Processors.Clip_Images_By_Extension(extension=extension),
+                                Processors.Distribute_into_2D()]
+    add_images_and_annotations = True
+    if not special_actions:
+        for processor in image_processors:
+            if type(processor) is Processors.Add_Images_And_Annotations:
+                add_images_and_annotations = False
+                break
+        if add_images_and_annotations:
+            image_processors = [Processors.Add_Images_And_Annotations()] + image_processors
+    if recordwriter is None:
+        recordwriter = RecordWriter(out_path=out_path, file_name_key='file_name', rewrite=rewrite)
+    threads = []
+    q = None
+    if not debug:
+        q = Queue(maxsize=thread_count)
+        a = [q, ]
+        for worker in range(thread_count):
+            t = Thread(target=worker_def, args=(a,))
+            t.start()
+            threads.append(t)
+    if file_parser is None:
+        data_dict = return_data_dict(niftii_path=niftii_path)
+    else:
+        data_dict = file_parser(**locals())
+    counter = 0
+    for iteration in data_dict.keys():
+        item = copy.deepcopy(data_dict[iteration])
+        input_item = OrderedDict()
+        input_item['input_features_dictionary'] = item
+        input_item['image_processors'] = image_processors
+        input_item['record_writer'] = recordwriter
+        input_item['verbose'] = verbose
+        if not debug:
+            q.put(input_item)
+        else:
+            serialize_example(**input_item)
+        counter += 1
+        if counter >= max_records:
+            break
+    if not debug:
+        for i in range(thread_count):
+            q.put(None)
+        for t in threads:
+            t.join()
+    return None
